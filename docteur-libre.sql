aea585f3d5253e3a8b1bd8dc468a89d1c5843eeb
Dorothee Huynh <dorothee.huynh@etu.paris.diderot.fr>
2019-04-11 21:41:28 +0200

    Revert "Ajout table User dans bdd (fichier sql) et reorganisation tables patients et docteur" : Contrainte d'intégrité réf. sur les id des patients et docteurs dans leurs tables respectives ne sont pas implementables dans doctrine
    This reverts commit bcd523cfbca9bde0edaa2ff95cc106e91a815ae8.

--- docteur-libre.sql
+++ docteur-libre.sql
@@ -3,33 +3,29 @@ CREATE DATABASE IF NOT EXISTS `docteur-libre` CHARACTER SET utf8 COLLATE utf8_un
 
 USE `docteur-libre`;
 
--- User table
-CREATE TABLE IF NOT EXISTS `user` (
-	`user_id` INT NOT NULL AUTO_INCREMENT,
+-- Patient table
+CREATE TABLE IF NOT EXISTS `patient` (
+	`patient_id` INT NOT NULL AUTO_INCREMENT,
 	`sex` CHAR(1) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
 	`first_name` VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
     `last_name` VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-    `phone_number` VARCHAR(16) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-    `password` CHAR(60) NOT NULL,
-	PRIMARY KEY (`user_id`)
-);
-
--- Patient table
-CREATE TABLE IF NOT EXISTS `patient` (
-	`patient_id` INT NOT NULL,
     `email` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+    `phone_number` VARCHAR(16) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
 	`date_of_birth` DATE NOT NULL,
-	PRIMARY KEY (`patient_id`),
-	FOREIGN KEY (`patient_id`) REFERENCES user(user_id)
+    `password` CHAR(60) NOT NULL,
+	PRIMARY KEY (`patient_id`)
 );
 
 -- Doctor table
 CREATE TABLE IF NOT EXISTS `doctor` (
 	`doctor_id` INT NOT NULL AUTO_INCREMENT,
+	`sex` CHAR(1) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+	`first_name` VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+    `last_name` VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
     `address` VARCHAR(40) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
     `profession` VARCHAR(40) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-	PRIMARY KEY (`doctor_id`),
-	FOREIGN KEY (`doctor_id`) REFERENCES user(user_id)
+    `password` CHAR(60) NOT NULL,
+	PRIMARY KEY (`doctor_id`)
 );
 
 -- Appointment table
